1b031cb3a54f9df6fa377ad84d3b11d82dcc9ac4
Evie Litherland-Smith <evie.litherland-smith@ukaea.uk>
2026-02-18 09:55:20 +0000

    Re-use some of my other flakes rather than make this monolithic

--- flake.nix
+++ flake.nix
@@ -1,65 +1,49 @@
 {
   description = "Collection of Python packages that aren't in nixpkgs";
+  inputs = {
+    imas-nix = {
+      url = "git+ssh://git@git.ccfe.ac.uk/elitherl/imas.nix?shallow=1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    calcam = {
+      url = "github:pixelifytica/calcam.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sal = {
+      url = "github:pixelifytica/simple-access-layer";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
   outputs =
-    { self, nixpkgs, ... }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: (nixpkgs.legacyPackages.${system}.extend self.overlays.default));
-    in
+    {
+      self,
+      nixpkgs,
+      imas-nix,
+      calcam,
+      sal,
+      ...
+    }:
     {
       overlays.default = (
         final: prev: {
-          pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-            (pfinal: pprev: {
-              corner = (pprev.corner.override { arviz = null; });
-              fortranformat = pfinal.callPackage ./fortranformat.nix { };
-              freeqdsk = pfinal.callPackage ./freeqdsk.nix { };
-              indica = pfinal.callPackage ./indica.nix { };
-              sal = pfinal.callPackage ./sal.nix { };
-              inference-tools = pfinal.callPackage ./inference-tools.nix { };
-              tokamesh = pfinal.callPackage ./tokamesh.nix { };
-              midas-fusion = pfinal.callPackage ./midas-fusion.nix { };
-              pyswarms = pfinal.callPackage ./pyswarms.nix { };
-              triangle = pfinal.callPackage ./triangle.nix { };
-              calcam = pfinal.callPackage ./calcam-lib.nix { };
-              imas-core = pfinal.callPackage ./imas-core.nix { };
-              imas-data-dictionaries = pfinal.callPackage ./imas-data-dictionaries.nix { };
-              imas-python = pfinal.callPackage ./imas-python.nix { };
-              imas-simdb = pfinal.callPackage ./imas-simdb.nix { };
-              imas-paraview = pfinal.callPackage ./imas-paraview.nix { };
-            })
-          ];
-          calcam-qt5 = final.callPackage ./calcam-gui.nix { useQt6 = false; };
-          calcam-qt6 = final.callPackage ./calcam-gui.nix { useQt6 = true; };
-          calcam = final.calcam-qt6;
-          simdb = final.python3Packages.toPythonApplication final.python3.pkgs.imas-simdb;
-          imas-paraview = final.python3Packages.toPythonApplication final.python3.pkgs.imas-paraview;
+          pythonPackagesExtensions =
+            prev.pythonPackagesExtensions
+            ++ (imas-nix.overlays.default final prev).pythonPackagesExtensions
+            ++ (calcam.overlays.default final prev).pythonPackagesExtensions
+            ++ (sal.overlays.default final prev).pythonPackagesExtensions
+            ++ [
+              (pfinal: pprev: {
+                corner = (pprev.corner.override { arviz = null; });
+                fortranformat = pfinal.callPackage ./fortranformat.nix { };
+                freeqdsk = pfinal.callPackage ./freeqdsk.nix { };
+                indica = pfinal.callPackage ./indica.nix { };
+                inference-tools = pfinal.callPackage ./inference-tools.nix { };
+                midas-fusion = pfinal.callPackage ./midas-fusion.nix { };
+                pyswarms = pfinal.callPackage ./pyswarms.nix { };
+                tokamesh = pfinal.callPackage ./tokamesh.nix { };
+              })
+            ];
         }
       );
-      packages = forAllSystems (system: {
-        inherit (pkgs.${system})
-          calcam-qt5
-          calcam-qt6
-          calcam
-          simdb
-          imas-paraview
-          ;
-      });
-      apps = forAllSystems (system: {
-        simdb = {
-          type = "app";
-          program = "${self.packages.${system}.simdb}/bin/simdb";
-          meta.description = "SimDB CLI";
-        };
-        calcam = {
-          type = "app";
-          program = "${self.packages.${system}.calcam}/bin/calcam";
-          meta.description = "Calcam";
-        };
-      });
     };
 }
