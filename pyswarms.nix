f9769323a178cf0244d5dea0620a9e8e5724258e
Evie Litherland-Smith <evie@xenia.me.uk>
2026-06-23 16:37:10 +0100

    Add future to pyswarms package
    Disable for Python 3.13 and newer due to future incompatibility

--- pyswarms.nix
+++ pyswarms.nix
@@ -1,18 +1,21 @@
 {
   buildPythonPackage,
   fetchPypi,
+  pythonAtLeast,
   scipy,
   numpy,
   matplotlib,
   attrs,
   tqdm,
   pyyaml,
+  future,
   ...
 }:
 buildPythonPackage rec {
   pname = "pyswarms";
   version = "1.3.0";
   format = "wheel";
+  disabled = pythonAtLeast "3.13";
   src = fetchPypi {
     inherit pname version format;
     hash = "sha256-ilHlX7wmsKZTiROg8kOcSBs4kXh7fqmMsswGP4NN0VQ=";
@@ -24,5 +27,6 @@ buildPythonPackage rec {
     attrs
     tqdm
     pyyaml
+    future
   ];
 }
